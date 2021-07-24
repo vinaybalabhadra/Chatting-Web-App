f045cf7395ab298372a63fbe705facd0b6a193ca
vinaybalabhadra <43163557+vinaybalabhadra@users.noreply.github.com>
2021-07-24 23:15:18 +0530

    Add files via upload

---
+++ javascript/chat.js
@@ -0,0 +1,63 @@
+const form = document.querySelector(".typing-area"),
+incoming_id = form.querySelector(".incoming_id").value,
+inputField = form.querySelector(".input-field"),
+sendBtn = form.querySelector("button"),
+chatBox = document.querySelector(".chat-box");
+
+form.onsubmit = (e)=>{
+    e.preventDefault();
+}
+
+inputField.focus();
+inputField.onkeyup = ()=>{
+    if(inputField.value != ""){
+        sendBtn.classList.add("active");
+    }else{
+        sendBtn.classList.remove("active");
+    }
+}
+
+sendBtn.onclick = ()=>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/insert-chat.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+              inputField.value = "";
+              scrollToBottom();
+          }
+      }
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
+}
+
+setInterval(() =>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/get-chat.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+            let data = xhr.response;
+            chatBox.innerHTML = data;
+            if(!chatBox.classList.contains("active")){ // only scroll to bottom if active is not in classlist of chatbox. Active is added in class list is mouse is scrolled and active is removed if mouse is left
+                scrollToBottom();
+              }
+          }
+      }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("incoming_id="+incoming_id);
+}, 500);
+
+function scrollToBottom(){      // this function is written to scroll the chat of users to scroll to the bottom so that recent messages are always visible first... user can scroll up to see previous messages
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }
+  
---
+++ javascript/users.js
@@ -0,0 +1,51 @@
+const searchBar = document.querySelector(".search input"),
+searchIcon = document.querySelector(".search button"),
+usersList = document.querySelector(".users-list");
+
+searchIcon.onclick = ()=>{
+  searchBar.classList.toggle("show");
+  searchIcon.classList.toggle("active");
+  searchBar.focus();
+  if(searchBar.classList.contains("active")){
+    searchBar.value = "";
+    searchBar.classList.remove("active");
+  }
+}
+
+searchBar.onkeyup = ()=>{     // onkeyup occurs when user releases a key
+  let searchTerm = searchBar.value;
+  if(searchTerm != ""){
+    searchBar.classList.add("active");
+  }else{
+    searchBar.classList.remove("active");
+  }
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "php/search.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+        if(xhr.status === 200){
+          let data = xhr.response;
+          usersList.innerHTML = data;
+        }
+    }
+  }
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("searchTerm=" + searchTerm);
+}
+
+setInterval(() =>{  // this function will run frequently after 500ms
+  let xhr = new XMLHttpRequest();  // creating XML Object
+  xhr.open("GET", "php/users.php", true); // we use GET Method because we need to recieve data, not sending
+  xhr.onload = ()=>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+        if(xhr.status === 200){
+          let data = xhr.response;
+          if(!searchBar.classList.contains("active")){   // this is done becoz set interval should not work when user is searching some name in search box.
+            usersList.innerHTML = data;    // The innerHTML property sets or returns the HTML content (inner HTML) of an element.
+          }
+        }
+    }
+  }
+  xhr.send();
+}, 500);
+
